f54284bbce618e6dc4c4e2b4dc6ca0e9c1d486c3
Shaquon <shaquonkelley@gmail.com>
2019-10-07 13:13:57 -0400

    Profiled the tuneup and fixed bottleneck.
    Used timeit and cProfile to see what function was slowing the code down. Found that the is_duplicate method was the root of the problem. Got the cumtime down to .005

--- tuneup.py
+++ tuneup.py
@@ -1,20 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Tuneup assignment"""
 
 __author__ = "Shaquon"
 
 import cProfile
+import io
 import pstats
-import functools
+from functools import wraps
+import timeit
 
 
-def profile(func):
+def profile_decorator(func):
     """A function that can be used as a decorator to measure performance"""
-    # You need to understand how decorators are constructed and used.
-    # Be sure to review the lesson material on decorators, they are used
-    # extensively in Django and Flask.
-    raise NotImplementedError("Complete this decorator function")
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        result = func(*args, **kwargs)
+        pr.disable()
+        ps = pstats.Stats(pr).sort_stats('cumulative')
+        ps.print_stats(10)
+        return result
+    return wrapped
 
 
 def read_movies(src):
@@ -31,21 +39,27 @@ def is_duplicate(title, movies):
             return True
     return False
 
-
+@profile_decorator
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
-    duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
-            duplicates.append(movie)
+    movies.sort()
+    # duplicates = []
+    # while movies:
+    #     movie = movies.pop()
+    #     if movie in movies:
+    #         duplicates.append(movie)
+    duplicates = [m1 for m1,m2 in zip(movies[1:], movies[:-1]) if m1 == m2]
+    print(duplicates)
     return duplicates
 
 
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
-    # YOUR CODE GOES HERE
+    t = timeit.Timer(stmt='main()', setup='text = "sample string"; char = "g"')
+    result = t.repeat(repeat=3, number=3)
+    avg_of_min = min([times/3 for times in result])
+    print('Best time across 3 repeats of 3 runs per repeat:{} sec'.format(avg_of_min))
 
 
 def main():
@@ -53,6 +67,7 @@ def main():
     result = find_duplicate_movies('movies.txt')
     print('Found {} duplicate movies:'.format(len(result)))
     print('\n'.join(result))
+    # timeit_helper()
 
 
 if __name__ == '__main__':
